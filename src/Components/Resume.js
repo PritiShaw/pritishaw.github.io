ea97af1b4e45eb96a6e97ed00a72fdc2307799a6
Priti Shaw <pritishaw0103@gmail.com>
2019-12-25 23:35:07 +0530

    Portfolio files upload

---
+++ src/Components/Resume.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+  render() {
+
+    if(this.props.data){
+      var skillmessage = this.props.data.skillmessage;
+      var work = this.props.data.work.map(function(work){
+        return <div key={work.company}><h3><a href={work.link}>{work.company}</a></h3>
+            <p className="info">{work.title} <em className="date">{work.years}</em></p>
+            <p>{work.description}</p>
+        </div>
+      })
+      var skills = this.props.data.skills.map(function(skills){
+        return <div key={skills.type}><h4>{skills.type}</h4>
+            <p className="info">{skills.name}</p>
+        </div>
+      })
+    }
+
+    return (
+      <section id="resume">
+      <div className="row work">
+         <div className="three columns header-col">
+            <h1><span>Projects</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+          {work}
+        </div>
+    </div>
+    <div className="row skill">
+
+         <div className="three columns header-col">
+            <h1><span>Skills</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+				<div className="bars">
+				   <ul className="skills">
+					  {skills}
+					</ul>
+				</div>
+			</div>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Resume;
